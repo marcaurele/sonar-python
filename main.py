2a5a0fd2ae6aa2676a1cefdc52711623be59a5cb
Marc-Aurèle Brothier <m@brothier.org>
2025-04-22 21:58:45 +0200

    ruff

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from typing import Deque, List
 
 from flask import Flask, request
 
+
 class Event:
     def __init__(self):
         self._peeked_events: Deque = deque()
         self._events: List = []
 
-    def store(self, event:str) -> List:
+    def store(self, event: str) -> List:
         events = [event]
         self._peeked_events.extend(events)
         self._events.append(event)
@@ -20,7 +21,8 @@ class Event:
 app = Flask(__name__)
 events = Event()
 
-@app.route("/", methods=('GET',))
+
+@app.route("/", methods=("GET",))
 def main_route() -> dict:
     event_key = request.args.get("event", "")
     return {"events": events.store(event_key)}
